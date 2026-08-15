8f1e5cc3758752c624806254f19c846729bef7dc
geobrgdps <geobrgdps@gmail.com>
2026-08-15 12:06:34 -0300

    feat: add configurable demon list data

---
+++ levels.js
@@ -0,0 +1,4 @@
+const LEVELS = [
+  // Configure the GeoBR List here.
+  // { position: 1, name: "Level Name", creator: "Creator", id: 123456, verifier: "Verifier", difficulty: "Extreme Demon", video: "https://...", status: "Published" }
+];
